81bc5c8badd128c188e10902b3d6ae71f0079a2e
SnehalTapade <snehu27aug@gmail.com>
2023-07-19 14:53:41 +0530

    all basic variable assignment 1
    all basic variable assignment 1

---
+++ 01_variableAssign.js
@@ -0,0 +1,34 @@
+
+var fullName;
+console.log(fullName);
+
+fullName = " Snehal Tapade";
+console.log(fullName);
+
+fullName = "Snehal Nandkishor Tapade"
+console.log(fullName);
+
+var myLove;
+myLove = " I love only JS "
+console.log(myLove);
+
+var firstName = "Snehal";
+console.log(firstName);
+
+var lastName = "Tapade";
+console.log(lastName);
+
+var age = 23;
+console.log(age);
+
+var address = "pune";
+console.log(address);
+
+var pinCode = 411057;
+console.log(pinCode);
+
+var gender = "Female";
+console.log(gender);
+
+var isMarried = "True";
+console.log(isMarried);
