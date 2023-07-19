5d5ada0db88df87853d6c0fa0ab2442ba9b5d083
SnehalTapade <snehu27aug@gmail.com>
2023-07-19 13:11:16 +0530

    basic JS programming
    basic JS programming

---
+++ 01_variable.js
@@ -0,0 +1,20 @@
+var myFirstName; // Variable Declaration
+
+myName = "Snehal"; // Variable Assignment or Initialization
+
+ 
+
+console.log(myName);
+
+ 
+
+var city = "Pune"; // Variable Declaration and Initialization
+
+console.log(city);
+
+ 
+
+var pin; // Variable Declaration
+
+console.log(pin);
+
