e6571f546a0e818f42318034678d348901b2bcf0
SnehalTapade <snehu27aug@gmail.com>
2023-07-19 15:11:30 +0530

    assignment of all primitive typesOf
    assignment of all primitive typesOf

---
+++ 02_typesOf.js
@@ -0,0 +1,30 @@
+
+var bankName;
+console.log(bankName);
+bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+
+var marks;
+console.log(marks);
+marks = '90%';
+console.log(marks);
+console.log(typeof marks);
+
+var isWorking;
+console.log(isWorking);
+isWorking = "True";
+console.log(isWorking);
+console.log(typeof isWorking);
+
+var totalCount = "Hundred and seven";
+console.log(totalCount);
+console.log(typeof totalCount);
+var totalCount= 107
+console.log(totalCount);
+console.log(typeof totalCount);
+
+
+
+
+
