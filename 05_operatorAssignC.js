a10a0ba46ae6ea033f45975342d2680445d3c389
SnehalTapade <snehu27aug@gmail.com>
2023-07-27 07:46:52 +0530

    incomplete

---
+++ 05_operatorAssignC.js
@@ -0,0 +1,5 @@
+var maleMarriageEligibility (gender,age,boyName){
+    gender>=21;
+}
+var maleMarriageEligibility ("male",25,Bilgates)
+console.log(` Hey $(maleMarriageEligibility`) ? you are eligible for marriage : not eligible for marriage);
