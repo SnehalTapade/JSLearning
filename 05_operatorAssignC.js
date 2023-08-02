ef827beb6cf42d5c5799ca9d796ac6132d800ae3
SnehalTapade <snehu27aug@gmail.com>
2023-08-02 12:24:32 +0530

    Operator assignment.js
    Operator assignment.js

--- 05_operatorAssignC.js
+++ 05_operatorAssignC.js
@@ -1,10 +1,24 @@
-var interviewEligibility = function(gradScore, hscScore, sscScore, candidateName ){
-    var result = gradScore>=70 || hscScore>= 80 || sscScore >=90 
-     ? `Congrats ${candidateName} you are eligible for TCS interview.`
-     :  `Unfortunately you are not eligible for interview`;
-     console.log(result);
- }
+// var interviewEligibility = function(gradScore, hscScore, sscScore, candidateName ){
+//     var result = gradScore>=70 || hscScore>= 80 || sscScore >=90 
+//      ? `Congrats ${candidateName} you are eligible for TCS interview.`
+//      :  `Unfortunately you are not eligible for interview`;
+//      console.log(result);
+//  }
  
- interviewEligibility(80, 86, 90, 'Mahi');
- interviewEligibility(70, 65, 55, 'Gill');
- interviewEligibility(60, 79, 88, 'Stew');
+//  interviewEligibility(80, 86, 90, 'Mahi');
+//  interviewEligibility(70, 65, 55, 'Gill');
+//  interviewEligibility(60, 79, 88, 'Stew');
+
+
+
+ function squareOfWordLength(){
+
+    // let text = "Hello World!";
+    // let length = text.length;
+    var str="JavaScript";
+    var length = str.length;
+
+ }
+ squareOfWordLength();
+//  squareOfWordLength("the length of JavaScript is : ${length}");
+//  squareOfWordLength("Google chrome");
