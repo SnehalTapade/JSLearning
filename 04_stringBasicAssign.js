51c16b5086986dc292e4d9965b3d7719c50d6e4e
SnehalTapade <snehu27aug@gmail.com>
2023-09-01 19:38:28 +0530

    String basic assignment completed
    String basic assignment completed

--- 04_stringBasicAssign.js
+++ 04_stringBasicAssign.js
@@ -1,9 +1,24 @@
 
 
-console.log(`My dream company is "Cognizant"`);
+// console.log(`My dream company is "Cognizant"`);
 
-var hobby1 = reading;
-var hobby2 = dancing;
-var hobby3 = playing;
+// var hobby1 = reading;
+// var hobby2 = dancing;
+// var hobby3 = playing;
 
-console.log( `${hobby1}my hobbies are  : ${hobby1} `)
+// console.log( `${hobby1}my hobbies are  : ${hobby1} `)
+
+
+function stringBasic(){
+    var greet = "My dream company is Cognizant";
+    console.log(greet);
+    var hobby1 = "watchingreels";
+    var hobby2 = "reading";
+    var hobby3 = "dancing";
+        console.log(`my hobbies are : ${hobby1}, ${hobby2}, ${hobby3}`);
+
+    var totalLength = hobby1.length + hobby2.length + hobby3.length
+    console.log(`total length is ${totalLength}`);
+    
+}
+stringBasic();
