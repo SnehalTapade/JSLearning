36cf8a7d57460d85211c424057dee31627773bf0
SnehalTapade <snehu27aug@gmail.com>
2023-09-04 21:42:43 +0530

    Ternary operator assignment
    Ternary operator assignment is complete

---
+++ 05_operatorAssignB.js
@@ -0,0 +1,29 @@
+
+var number= function(num1, num2){
+
+        var num1 = 800;
+        var num2 = 899;
+
+       var result =  (num1 > num2) ? "num1 is greater" : "num2 is greater";
+       console.log(result);
+
+
+}
+number(10,-10);
+number(800,899);
+
+console.log(`=================================`);
+
+var num = function(num1){
+
+       var isEvenOrOdd = (num1%2==0) ? "given number is even" : "given number is odd ";
+       console.log(isEvenOrOdd);
+
+}
+num(29);
+num(44);
+num(0);
+num(101);
+
+
+
