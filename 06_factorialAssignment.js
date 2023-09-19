1e099a088852dcf4049ba6575fea0dca2ca0cce4
SnehalTapade <snehu27aug@gmail.com>
2023-09-19 14:28:04 +0530

    NA
    NA

--- 06_factorialAssignment.js
+++ 06_factorialAssignment.js
@@ -1,12 +1,21 @@
 
 
 //  WAP to find the factorial of numbers: 5, 9, 11, 7 using for loop
-console.log(`===============factorial 1=======================`);
-let result = 1;
-for (let index = 1; index <=5; index++) {
+// console.log(`===============factorial 1=======================`);
+// let result = 1;
+// for (let index = 1; index <=5; index++) {
     
-    result = result * index;
+//     result = result * index;
+// }
+// console.log(`Factorial of 5 is ${result}`);
+
+let results = 1;
+function fact (fact){
+    
+ for (let index = 1; index =5; index++) {
+        results= results*index;
+     }
+
 }
-console.log(`Factorial of 5 is ${result}`);
-
-
+let fact = fact(5);
+console.log(`factoral of 5 is : ${fact}`);
